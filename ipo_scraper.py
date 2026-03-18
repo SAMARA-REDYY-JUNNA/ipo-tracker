319eae4ff292c9525010899ab25aed9b7aa0bdf6
samarareddy <samara.junna@gmail.com>
2026-03-18 12:47:03 +0530

    functions

--- ipo_scraper.py
+++ ipo_scraper.py
@@ -5,18 +5,27 @@ def get_ipos():
 
     try:
         response = requests.get(url)
+
+        # Debug print
+        print("RAW RESPONSE:", response.text[:200])
+
         data = response.json()
 
         ipo_list = []
 
-        for item in data:
-            ipo = {
-                "name": item.get("companyName", ""),
-                "open_date": item.get("openDate", ""),
-                "close_date": item.get("closeDate", ""),
-                "price": item.get("priceBand", "")
-            }
-            ipo_list.append(ipo)
+        # Ensure data is list
+        if isinstance(data, list):
+            for item in data:
+                if isinstance(item, dict):
+                    ipo = {
+                        "name": item.get("companyName", ""),
+                        "open_date": item.get("openDate", ""),
+                        "close_date": item.get("closeDate", ""),
+                        "price": item.get("priceBand", "")
+                    }
+                    ipo_list.append(ipo)
+        else:
+            print("Unexpected data format:", type(data))
 
         return ipo_list
 
